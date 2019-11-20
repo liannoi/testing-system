8e4dc9f74c3abada2d8d8edb858bcf2f181dbf93
Maksym Liannoi <liannoi444@gmail.com>
2019-11-20 17:19:41 +0200

    Described the most generalized class library that can be used in any projects and for any entities

---
+++ src/Multilayer.DataServices/DataServices/BaseDataService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Multilayer.DataServices
+{
+    public abstract class BaseDataService<TEntity> : IDataService<TEntity> where TEntity : class, new()
+    {
+        private readonly DbContext context;
+        private readonly IDbSet<TEntity> entities;
+
+        public BaseDataService(DbContext context)
+        {
+            this.context = context;
+            entities = context.Set<TEntity>();
+        }
+
+        public virtual void AddOrUpdate(TEntity entity)
+        {
+            entities.AddOrUpdate(entity);
+        }
+
+        public virtual int Commit()
+        {
+            return context.SaveChanges();
+        }
+
+        public virtual void Remove(TEntity entity)
+        {
+            entities.Remove(entity);
+        }
+
+        public virtual IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> expression)
+        {
+            return entities.Where(expression);
+        }
+
+        public virtual IEnumerable<TEntity> Select()
+        {
+            return entities;
+        }
+
+        public virtual TEntity Select(int id)
+        {
+            return entities.Find(id);
+        }
+    }
+}
---
+++ src/Multilayer.DataServices/DataServices/IDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Multilayer.DataServices
+{
+    public interface IDataService<TEntity> where TEntity : class, new()
+    {
+        void AddOrUpdate(TEntity entity);
+        IEnumerable<TEntity> Select();
+        TEntity Select(int id);
+        void Remove(TEntity entity);
+        int Commit();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> expression);
+    }
+}
