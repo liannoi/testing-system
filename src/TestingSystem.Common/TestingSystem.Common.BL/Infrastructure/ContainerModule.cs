218a9b86cc4b3570de14e876c2d49a6aeeea6375
Maksym Liannoi <liannoi444@gmail.com>
2019-11-21 06:04:04 +0200

    Deployed dependencies through Autofac

---
+++ src/TestingSystem.Common/TestingSystem.Common.BL/Infrastructure/ContainerModule.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using Multilayer.BusinessServices;
+using Multilayer.DataServices;
+using System.Data.Entity;
+using TestingSystem.Common.BL.BusinessObjects;
+using TestingSystem.Common.BL.BusinessServices;
+using TestingSystem.Common.DAL.DataObjects;
+using TestingSystem.Common.DAL.DataServices;
+
+namespace TestingSystem.Common.BL.Infrastructure
+{
+    public sealed class ContainerModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // DAL.
+            builder.RegisterType(typeof(AnswersMarkableDataService)).As(typeof(IDataService<Answer>));
+            builder.RegisterType(typeof(EntitiesContext)).As(typeof(DbContext));
+
+            // BL.
+            builder.RegisterType(typeof(AnswersBusinessService)).As(typeof(IBusinessService<AnswerBusinessObject>));
+        }
+    }
+}
