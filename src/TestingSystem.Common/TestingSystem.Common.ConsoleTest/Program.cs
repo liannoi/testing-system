081dfcf4960c2a70e44241212aa425a1e491b49d
Maksym Liannoi <liannoi444@gmail.com>
2019-11-21 06:02:43 +0200

    Added a draft business logic console test project

---
+++ src/TestingSystem.Common/TestingSystem.Common.ConsoleTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Multilayer.BusinessServices;
+using TestingSystem.Common.BL.BusinessObjects;
+using TestingSystem.Common.BL.Infrastructure;
+
+namespace TestingSystem.Common.ConsoleTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ContainerConfig container = new ContainerConfig();
+            IBusinessService<AnswerBusinessObject> businessService = container.Container.Resolve<IBusinessService<AnswerBusinessObject>>();
+            businessService.Remove(new AnswerBusinessObject
+            {
+                AnswerId = 7
+            });
+        }
+    }
+}
