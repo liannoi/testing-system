34eeed68a881efc347e731e5d3393852ab48d811
Maksym Liannoi <liannoi444@gmail.com>
2019-11-21 11:15:43 +0200

    Refactoring, preparing for dependency injection

--- src/TestingSystem.Common/TestingSystem.Common.BL/Infrastructure/ContainerModule.cs
+++ src/TestingSystem.Common/TestingSystem.Common.BL/Infrastructure/ContainerModule.cs
@@ -15,13 +15,44 @@ namespace TestingSystem.Common.BL.Infrastructure
     {
         protected override void Load(ContainerBuilder builder)
         {
-            // DAL.
-            builder.RegisterType(typeof(BaseDatabaseDataService<Answer>))
-                .As(typeof(IDataService<Answer>));
-            builder.RegisterType(typeof(EntitiesContext))
-                .As(typeof(DbContext));
+            #region Dependency injection at the Data Access Level.
 
-            // BL.
+            // Context.
+            InjectContext(builder);
+
+            // Answer Data Service.
+            InjectAnswersDataService(builder);
+
+            // Group Data Service.
+            // Group Test Data Service.
+            // Question.
+            // Role Test.
+            // Student Test.
+            // Test.
+            // User.
+            // User Role.
+
+            #endregion
+
+            #region Dependency injection at the Business Logic Level.
+
+            // Answer Business Service.
+            InjectAnswersBusinessService(builder);
+
+            // Group Business Service.
+            // Group Test Business Service.
+            // Question.
+            // Role Test.
+            // Student Test.
+            // Test.
+            // User.
+            // User Role.
+
+            #endregion
+        }
+
+        private static void InjectAnswersBusinessService(ContainerBuilder builder)
+        {
             builder.RegisterType(typeof(BusinessServices.BaseBusinessService<Answer, AnswerBusinessObject>))
                 .As(typeof(IBusinessService<AnswerBusinessObject>))
                 .WithParameter("mapper", new MapperConfiguration(cfg =>
@@ -31,6 +62,18 @@ namespace TestingSystem.Common.BL.Infrastructure
                        .ForMember(nameof(AnswerBusinessObject.QuestionTitle), o => o.MapFrom(s => s.Question.Text));
                     cfg.CreateMap<AnswerBusinessObject, Answer>();
                 }).CreateMapper());
+        }
+
+        private static void InjectAnswersDataService(ContainerBuilder builder)
+        {
+            builder.RegisterType(typeof(BaseDatabaseDataService<Answer>))
+                .As(typeof(IDataService<Answer>));
+        }
+
+        private static void InjectContext(ContainerBuilder builder)
+        {
+            builder.RegisterType(typeof(EntitiesContext))
+                .As(typeof(DbContext));
         }
     }
 }
