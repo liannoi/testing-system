89839c33eb981e86dddf608a5b09ea0a5e6115ec
Maksym Liannoi <liannoi444@gmail.com>
2019-11-24 09:59:03 +0200

    Described the basic components of all ViewModels for WPF applications

---
+++ src/helpers/Client.Desktop.BL/Infrastructure/BaseViewModel.cs
@@ -0,0 +1,107 @@
+﻿using Client.Desktop.BL.Infrastructure.Helpers;
+using System;
+using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Client.Desktop.BL.Infrastructure
+{
+    public class BaseViewModel : Bindable, IDisposable
+    {
+        private readonly CancellationTokenSource cancellationTokenSource;
+        private readonly ConcurrentDictionary<string, ICommand> cachedCommands;
+
+        public bool IsLoadDataStarted
+        {
+            get => Get<bool>();
+            protected internal set => Set(value);
+        }
+
+        public CancellationToken CancellationToken => cancellationTokenSource?.Token ?? CancellationToken.None;
+
+        public BaseViewModel()
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+            cachedCommands = new ConcurrentDictionary<string, ICommand>();
+        }
+
+        ~BaseViewModel()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void LoadData()
+        {
+            if (IsLoadDataStarted)
+            {
+                return;
+            }
+
+            IsLoadDataStarted = true;
+            Task.Run(LoadDataAsync, CancellationToken);
+        }
+
+        public void CancelRequests()
+        {
+            cancellationTokenSource.Cancel();
+        }
+
+        protected ICommand MakeCommand(Action<object> commandAction, [CallerMemberName] string propertyName = null)
+        {
+            return GetCommand(propertyName) ?? SaveCommand(new RelayCommand(commandAction), propertyName);
+        }
+
+        protected ICommand MakeCommand(Action<object> commandAction, Func<object, bool> func, [CallerMemberName] string propertyName = null)
+        {
+            return GetCommand(propertyName) ?? SaveCommand(new RelayCommand(commandAction, func), propertyName);
+        }
+
+        /// <summary>
+        /// Override this method for load data.
+        /// </summary>
+        protected virtual Task LoadDataAsync()
+        {
+            return Task.FromResult(0);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            CancelRequests();
+        }
+
+        // TODO: Describe non-statistical methods that would show download windows, hiding them. Also, you need to describe static methods for displaying messages for the client ("Are you sure ...?").
+
+        private ICommand SaveCommand(ICommand command, string propertyName)
+        {
+            if (string.IsNullOrEmpty(propertyName))
+            {
+                throw new ArgumentNullException(nameof(propertyName));
+            }
+
+            if (!cachedCommands.ContainsKey(propertyName))
+            {
+                cachedCommands.TryAdd(propertyName, command);
+            }
+
+            return command;
+        }
+
+        private ICommand GetCommand(string propertyName)
+        {
+            if (string.IsNullOrEmpty(propertyName))
+            {
+                throw new ArgumentNullException(nameof(propertyName));
+            }
+
+            return cachedCommands.TryGetValue(propertyName, out ICommand cachedCommand) ? cachedCommand : null;
+        }
+    }
+}
---
+++ src/helpers/Client.Desktop.BL/Infrastructure/Helpers/RelayCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Client.Desktop.BL.Infrastructure.Helpers
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> action;
+        private readonly Func<object, bool> func;
+
+        public RelayCommand(Action<object> action)
+        {
+            this.action = action;
+        }
+
+        public RelayCommand(Action<object> action, Func<object, bool> func)
+        {
+            this.action = action;
+            this.func = func;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return func == null || func(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            action(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+    }
+}
