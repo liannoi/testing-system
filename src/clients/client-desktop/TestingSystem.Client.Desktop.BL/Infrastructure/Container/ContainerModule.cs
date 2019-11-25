8de9b1272debbe7c78e7e41d9be10254bfe4ff30
Maksym Liannoi <liannoi444@gmail.com>
2019-11-25 09:19:12 +0200

    Refactored dependency injection methods

--- src/clients/client-desktop/TestingSystem.Client.Desktop.BL/Infrastructure/Container/ContainerModule.cs
+++ src/clients/client-desktop/TestingSystem.Client.Desktop.BL/Infrastructure/Container/ContainerModule.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac;
+using System;
 using TestingSystem.Client.Desktop.BL.Infrastructure.Validators;
 
 namespace TestingSystem.Client.Desktop.BL.Infrastructure.Container
@@ -7,24 +8,32 @@ namespace TestingSystem.Client.Desktop.BL.Infrastructure.Container
     {
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterType(typeof(LoginValidator))
-                .As(typeof(ILoginValidator))
-                .WithParameter("validatorParameter", new StringValidatorParameter
-                {
-                    MinLength = 8,
-                    MaxLength = 128
-                });
+            Inject(builder, typeof(LoginValidator), typeof(ILoginValidator), "validatorParameter", new StringValidatorParameter
+            {
+                MinLength = 8,
+                MaxLength = 128
+            });
 
-            builder.RegisterType(typeof(PasswordValidator))
-                .As(typeof(IPasswordValidator))
-                .WithParameter("validatorParameter", new StringValidatorParameter
-                {
-                    MinLength = 5,
-                    MaxLength = 128
-                });
+            Inject(builder, typeof(PasswordValidator), typeof(IPasswordValidator), "validatorParameter", new StringValidatorParameter
+            {
+                MinLength = 5,
+                MaxLength = 128
+            });
 
-            builder.RegisterType(typeof(StringValidatorParameter))
-                .As(typeof(IStringValidatorParameter));
+            Inject(builder, typeof(StringValidatorParameter), typeof(IStringValidatorParameter));
+        }
+
+        private void Inject(ContainerBuilder builder,Type registerType, Type asType, string parameterName, IStringValidatorParameter parameter)
+        {
+            builder.RegisterType(registerType)
+                .As(asType)
+                .WithParameter(parameterName, parameter);
+        }
+
+        private void Inject(ContainerBuilder builder, Type registerType, Type asType)
+        {
+            builder.RegisterType(registerType)
+                .As(asType);
         }
     }
 }
