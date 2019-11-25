d93146b9490102b43e7ff56f7ea4cb23599682cb
Maksym Liannoi <liannoi444@gmail.com>
2019-11-25 12:03:57 +0200

    [Intermediate] Student Window dashboard business logic description

---
+++ src/clients/client-desktop/TestingSystem.Client.Desktop.BL/BusinessServices/Tests/TestsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingSystem.Client.Desktop.BL.BusinessServices.Tests
+{
+    public class TestsService
+    {
+        // TODO: According to UserId, get absolutely all tests.
+    }
+}
---
+++ src/clients/client-desktop/TestingSystem.Client.Desktop.BL/Infrastructure/ViewModels/Student/StudentDashboardViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Client.Desktop.BL.Infrastructure;
+
+namespace TestingSystem.Client.Desktop.BL.Infrastructure.ViewModels.Student
+{
+    public sealed class StudentDashboardViewModel : BaseViewModel
+    {
+        // TODO: Inject Autofac.
+    }
+}
