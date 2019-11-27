c57c661100238cf6b84f9f882d60d63306c0032f
Maksym Liannoi <liannoi444@gmail.com>
2019-11-27 18:41:10 +0200

    Prepared to describe the test-taking window

--- src/clients/client-desktop/TestingSystem.Client.Desktop/BL/Infrastructure/ViewModels/Student/StudentTestDetailsViewModel.cs
+++ src/clients/client-desktop/TestingSystem.Client.Desktop/BL/Infrastructure/ViewModels/Student/StudentTestDetailsViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using Client.Desktop.BL.Infrastructure;
+using System;
+using System.Windows.Input;
 using TestingSystem.Common.BL.BusinessObjects;
 
 namespace TestingSystem.Client.Desktop.UI.BL.Infrastructure.ViewModels.Student
 {
     public class StudentTestDetailsViewModel : BaseViewModel
     {
+        #region Properties
+
         public TestBusinessObject Test
         {
             get => Get<TestBusinessObject>();
@@ -17,10 +21,31 @@ namespace TestingSystem.Client.Desktop.UI.BL.Infrastructure.ViewModels.Student
             set => Set(value);
         }
 
+        #endregion
+
+        #region Commands
+
+        public ICommand StartTestCommand => MakeCommand(a => StartTest(), c => TestDetails.AllowToPass);
+
+        #endregion
+
+        #region Constructors
+
         public StudentTestDetailsViewModel(TestBusinessObject test, StudentTestBusinessObject testDetails)
         {
             Test = test;
             TestDetails = testDetails;
         }
+
+        #endregion
+
+        #region Commands implementation
+
+        private void StartTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
