7945f8ea8f0ddf093e548f224080f9858d85dab1
Maksym Liannoi <liannoi444@gmail.com>
2019-12-02 17:57:46 +0200

    An exception

---
+++ src/clients/client-desktop/TestingSystem.Client.Desktop/BL/BusinessServices/PassingTest/TestQuestionsOverException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TestingSystem.Client.Desktop.BL.BusinessServices.PassingTest
+{
+    [Serializable]
+    public class TestQuestionsOverException : Exception
+    {
+        public TestQuestionsOverException() : base("Questions are over.") { }
+        public TestQuestionsOverException(string message) : base(message) { }
+        public TestQuestionsOverException(string message, Exception inner) : base(message, inner) { }
+        protected TestQuestionsOverException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
